3ac658a09be151535750fcb7b344e6a658efae48
KrapuRED <vladimiredo50@gmail.com>
2026-07-04 17:21:04 +0700

    Camera Fix update, Dropbox sprite

--- Assets/Scripts/CameraFollow/CursorYFollowTarget.cs
+++ Assets/Scripts/CameraFollow/CursorYFollowTarget.cs
@@ -2,36 +2,66 @@ using UnityEngine;
 
 public class CursorYFollowTarget : MonoBehaviour
 {
-    [Header("Range")]
-    [SerializeField] private float maxOffsetY = 5f; // clamp how far up/down this target can travel
+    [Header("References")]
+    [SerializeField] private Camera cam;
 
-    private float _originY;
+    [Header("Edge Scroll")]
+    [Tooltip("How much of the top/bottom of the screen triggers scrolling.")]
+    [Range(0.05f, 0.4f)]
+    [SerializeField] private float edgeSize = 0.18f;
 
-    private GamePlayInput _gamePlayInput;
+    [Tooltip("Maximum look-ahead distance.")]
+    [SerializeField] private float maxOffset = 5f;
+
+    [Header("Smoothing")]
+    [Tooltip("Lower = snappier, Higher = smoother")]
+    [SerializeField] private float smoothTime = 0.08f;
+
+    private GamePlayInput input;
+    private float velocityY;
 
     private void Awake()
     {
-        _gamePlayInput = new GamePlayInput();
-        _originY = transform.position.y;
+        input = new GamePlayInput();
+
+        if (cam == null)
+            cam = Camera.main;
     }
 
-    private void OnEnable()
-    {
-        _gamePlayInput.Enable();
-    }
+    private void OnEnable() => input.Enable();
+    private void OnDisable() => input.Disable();
 
-    private void OnDisable()
+    private void LateUpdate()
     {
-        _gamePlayInput.Disable();
-    }
+        Vector2 mouse = input.Mouse.Position.ReadValue<Vector2>();
 
-    private void FixedUpdate()
-    {
-        Vector2 screenPos = _gamePlayInput.Mouse.Position.ReadValue<Vector2>();
-        float normalizedY = (screenPos.y / Screen.height - 0.5f) * 2f;
+        float viewportY = mouse.y / Screen.height;
+
+        float desiredOffset = 0f;
+
+        // Top edge
+        if (viewportY > 1f - edgeSize)
+        {
+            float t = Mathf.InverseLerp(1f - edgeSize, 1f, viewportY);
+            desiredOffset = t * maxOffset;
+        }
+        // Bottom edge
+        else if (viewportY < edgeSize)
+        {
+            float t = Mathf.InverseLerp(edgeSize, 0f, viewportY);
+            desiredOffset = -t * maxOffset;
+        }
+
+        // Always stay offset relative to the camera's CURRENT position.
+        float targetY = cam.transform.position.y + desiredOffset;
 
         Vector3 pos = transform.position;
-        pos.y = _originY + normalizedY * maxOffsetY;
+        pos.y = Mathf.SmoothDamp(
+            pos.y,
+            targetY,
+            ref velocityY,
+            smoothTime);
+
         transform.position = pos;
     }
 }
